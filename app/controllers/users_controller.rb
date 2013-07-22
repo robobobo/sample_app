e7ef102f6a5a8fa18a9e0647117e89dd9c978f6f
Robert Boland <robertboland1@gmail.com>
2013-07-23 00:26:59 +0100

    Finish sign in, some issues with cookies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
+ def show
+    @user = User.find(params[:id])
   end
 
-  def show
-  	@user = User.find(params[:id])
+  def new
+    @user = User.new
   end
 
   def create
-  	@user = User.new(params[:user])
-  	if @user.save
-  		flash[:success] = "Welcome to my Sample App!"
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-  	else 
-  		render 'new'
-  	end
-  end
+    else
+      render 'new'
+    end
+end
 end
