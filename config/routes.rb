82b1a9c592c482c37ec16cfa76d85a038940df48
Robert Boland <robertboland1@gmail.com>
2013-08-07 23:27:59 +0100

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 SampleApp::Application.routes.draw do
-resources :users
+resources :users do 
+  member do
+    get :following, :followers
+  end
+end
+
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
@@ -13,6 +20,7 @@ root to: 'static_pages#home'
   match '/contact', to:'static_pages#contact'
 match '/signup', to:'users#new'
 
+ 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -21,4 +21,13 @@ namespace :db do
     end
     end
   end
+
+  def make_relationships
+  users = User.all
+  user  = users.first
+  followed_users = users[2..50]
+  followers      = users[3..40]
+  followed_users.each { |followed| user.follow!(followed) }
+  followers.each      { |follower| follower.follow!(user) }
+end
 end
